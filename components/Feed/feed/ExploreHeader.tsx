0880eb08be4b02cb498f5dd3d3b4f0b3c3815718
Watcher <watchereth10@gmail.com>
2024-03-02 20:51:00 -0800

    Added Explore Header

--- components/Feed/feed/ExploreHeader.tsx
+++ components/Feed/feed/ExploreHeader.tsx
@@ -1,12 +1,36 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text, Dimensions } from "react-native";
+import { FlashList } from "@shopify/flash-list";
 
-const ExploreHeader = () => {
+const { width } = Dimensions.get("window");
+
+const ExploreHeader = ({ data }) => {
+  const halfLength = Math.ceil(data.length / 2);
+  const topData = data.slice(0, halfLength);
+  const bottomData = data.slice(halfLength);
+
+  const renderItem = ({ item }) => (
+    <View style={{ padding: 10, alignItems: "center" }}>
+      <Text style={{ color: "white" }}>{item}</Text>
+    </View>
+  );
+
   return (
-    <View
-      style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
-    >
-      <Text>ExploreHeader</Text>
+    <View>
+      <FlashList
+        data={topData}
+        renderItem={renderItem}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={{ maxHeight: 50, overflow: "hidden" }}
+      />
+      <FlashList
+        data={bottomData}
+        renderItem={renderItem}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={{ maxHeight: 50, overflow: "hidden", marginTop: 10 }}
+      />
     </View>
   );
 };
